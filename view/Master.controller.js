06f92dca95e07e489b04532876c2cb4f017723d2
Jason Scott <jason@jaylin.com.au>
2014-03-21 16:08:01 +0800

    Update Master.controller.js

--- view/Master.controller.js
+++ view/Master.controller.js
@@ -73,10 +73,10 @@
 						setTimeout(function() {
 							var listItemSelector = "#__item0-idViewRoot--idViewMaster--list-" + rowsToScroll;
 							if (this.usingIScroll) {
-								this.scroller.scrollToElement(listItemSelector, 300);
-								setTimeout(function() {
-									this.scroller.scrollTo(0, 20, 500);
-								}, 200);
+								this.scroller.scrollToElement(listItemSelector, 300, true, true);
+								//setTimeout(function() {
+								//	this.scroller.scrollTo(0, 20, 500);
+								//}, 200);
 							} else {
 								var offset = $(listItemSelector).position().top;
 								this.scroller.scrollTo(0, offset, 300);
