7837780c380bc8264567d8386af27a412d16ce50
Jason Scott <jason@jaylin.com.au>
2014-03-21 16:41:03 +0800

    add a message toast at end of auto-scroll after pull-to-refresh

--- view/Master.controller.js
+++ view/Master.controller.js
@@ -3,6 +3,7 @@
 
 	jQuery.sap.require("sap.ui.demo.util.Formatter");
 	jQuery.sap.require("sap.ui.demo.util.Grouper");
+	jQuery.sap.require("sap.m.MessageToast");
 
 	sap.ui.controller("sap.ui.demo.view.Master", {
 
@@ -63,7 +64,7 @@
 				var actualRows = evt.getParameter("actual");
 
 				if (this.listInit && actualRows > this.currentRows) {
-					var diff = actualRows - this.currentRows - 1;
+					var diff = actualRows - this.currentRows; // - 1;
 					var rowsToScroll = diff < 0 ? 0 : diff;
 
 					// Get scroller
@@ -72,12 +73,15 @@
 					if (this.scroller) {
 						setTimeout(function() {
 							var listItemSelector = "#__item0-idViewRoot--idViewMaster--list-" + rowsToScroll;
+							var offset;
 							if (this.usingIScroll) {
-								this.scroller.scrollToElement(listItemSelector, 200);
+								this.scroller.scrollToElement(listItemSelector, 400);
 							} else {
-								var offset = $(listItemSelector).position().top + 120;
-								this.scroller.scrollTo(0, offset, 300);
+								offset = $(listItemSelector).position().top;
+								this.scroller.scrollTo(0, offset, 400);
 							}
+
+							sap.m.MessageToast.show("Scroll up to see new items...");
 						}.bind(this), 200);
 					}
 				}
